1756f6debce8c67358cd3cb5c79cf1d41deb470d
hiren223 <123714322+hiren223@users.noreply.github.com>
2025-02-25 09:50:21 +0000

    done

--- conversationOperation.js
+++ conversationOperation.js
@@ -1,3 +1,4 @@
+// conversation:
 // let score = "hiren"
 
 // console.log(typeof score);
@@ -7,11 +8,9 @@
 // console.log(typeof vlaueInNumber);
 // console.log(vlaueInNumber);
 
-
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false =>0
-
 
 // let isLoggedIn = "Hiren"
 
@@ -22,11 +21,60 @@
 // "" => false
 // "Hiren"= true
 
+// let someNumber = 33
 
-let someNumber = 33
+// let stringNumber = String(someNumber)
 
-let stringNumber = String(someNumber)
+// console.log(stringNumber);
 
-console.log(stringNumber);
+// console.log(typeof stringNumber);
 
-console.log(typeof stringNumber);
+// Operations:
+
+let value = 3;
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello";
+let str2 = " Hiren";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2;
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+
+let x = 3;
+const y = x++;
+// x is 4; y is 3
+console.log(`x:${x}, y:${y}`);
+
+let x2 = 3n;
+const y2 = x2++;
+// x2 is 4n; y2 is 3n
+console.log(`x:${x2}, y:${y2}`);
+
