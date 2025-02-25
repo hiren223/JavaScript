7c7b8394bccdef159fe0dbe148cb8cec7e1e4846
hiren223 <123714322+hiren223@users.noreply.github.com>
2025-02-25 08:07:03 +0000

    done

---
+++ conversationOperation.js
@@ -0,0 +1,32 @@
+// let score = "hiren"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let vlaueInNumber = Number(score)
+// console.log(typeof vlaueInNumber);
+// console.log(vlaueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0
+
+
+// let isLoggedIn = "Hiren"
+
+// let booleanIsLoggedIn =Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Hiren"= true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
