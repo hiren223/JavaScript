de61e216d8a9cad2cddf6337518e56435fdee82f
hiren223 <123714322+hiren223@users.noreply.github.com>
2025-02-25 10:39:14 +0000

    done

---
+++ dataTypeSummery.js
@@ -0,0 +1,35 @@
+// Primitive Data Type
+
+// 7 types : String, Number, Boolean, null, Undefind, Symbol,BigInt
+
+const score = 100; //number
+const scoreValue = 100.3 //number
+
+const isLoggedIn = false //boolean
+const outsideTemp = null //null  //value: object
+let useremail; //undefind
+
+const id = Symbol('123') //symbol
+const anotherId =Symbol('123') //symbol
+// console.log(id === anotherId);
+
+const bigNumber = 862831624927401n
+
+
+// Reference (Non primitive data type)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj"] //array
+
+const Object = { name :"hiren", age : 20,} //object
+
+const myfunction = function(){
+    console.log("Hello World!");
+}
+console.log(typeof myfunction);
+
+myfunction();
+
+
+
