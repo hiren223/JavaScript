7a183dcd813c9a3952f0b6855d5b20d5102c8005
hiren223 <123714322+hiren223@users.noreply.github.com>
2025-03-03 12:16:32 +0000

    dene

---
+++ control_flow/one.js
@@ -0,0 +1,82 @@
+//  if
+
+const isUserloggedin = true
+const temperature = 41
+
+// if (temperature < 50) {
+//         console.log("less than 50");
+// }
+// else{
+//     console.log("temperature is greater than 50");
+// }
+
+
+// const score  =200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User power: ${power}`); 
+
+// }
+
+// const balance = 1000
+
+// if (balance > 500) console.log("tets"),console.log("test2");  // do not write it
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// } else {
+//     console.log("less than 1200");
+// }
+
+// const userloggedin =true
+// const debitecard =false
+// const loggedinFromgoogle = false
+// const loggedinFromemail = true
+
+// if (userloggedin && debitecard) {
+//     console.log("Allow to buy course");  
+// }
+
+// if (loggedinFromgoogle || loggedinFromemail) {
+//     console.log("User logged in");
+    
+// }
+
+
+// switch case
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }  // syntax 
+
+
+const month =3
+
+switch (month) {
+    case 1:
+        console.log("January"); 
+        break;
+    case 2:
+        console.log("Feb"); 
+        break;
+    case 3:
+        console.log("March"); 
+        break;
+    case 4:
+        console.log("April"); 
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
