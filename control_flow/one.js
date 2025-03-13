d4a0dafce2519e698c43527c724f9ce1235035d3
hiren223 <123714322+hiren223@users.noreply.github.com>
2025-03-13 04:57:43 +0000

    done

--- control_flow/one.js
+++ control_flow/one.js
@@ -60,23 +60,3 @@ const temperature = 41
 // }  // syntax 
 
 
-const month =3
-
-switch (month) {
-    case 1:
-        console.log("January"); 
-        break;
-    case 2:
-        console.log("Feb"); 
-        break;
-    case 3:
-        console.log("March"); 
-        break;
-    case 4:
-        console.log("April"); 
-        break;
-
-    default:
-        console.log("default case match");
-        break;
-}
